ee43321c8c78b24680a29e5e97852030c14475b5
v-lavrushko <landadang@gmail.com>
2018-12-25 23:00:04 +0200

    First test of HeroesGame

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,8 +1,8 @@
 public class GameManager {
     void Fight(Character c1, Character c2) {
-        System.out.printf("Starting conditions:\n\t%s\n\t%s", c1, c2);
+        System.out.printf("Starting conditions:\n\t%s\n\t%s\n", c1, c2);
         c1.kick(c2);
         c2.kick(c1);
-        System.out.printf("After battle:\n\t%s\n\t%s", c1, c2);
+        System.out.printf("After battle:\n\t%s\n\t%s\n", c1, c2);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        GameManager gameManager = new GameManager();
+        CharacterFactory spawn = new CharacterFactory();
+        Character[] playingCharacters = new Character[5];
+        for (int i = 0; i < 5; i++) {
+            Character c = spawn.createCharacter();
+            System.out.println(c);
+            playingCharacters[i] = c;
+        }
+        Random random = new Random();
+        Character c1 = playingCharacters[random.nextInt(5)];
+        Character c2;
+        do {
+            c2 = playingCharacters[random.nextInt(5)];
+        } while (c2 == c1);
+        gameManager.Fight(c1, c2);
+        for (Character c : playingCharacters) {
+            System.out.println(c);
+        }
     }
 }
